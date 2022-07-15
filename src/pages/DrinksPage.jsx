567a9d10245403685957face49ccc15ae241a563
Pamela <abrepamm@gmail.com>
2022-07-15 18:36:17 -0300

    lint

--- src/pages/DrinksPage.jsx
+++ src/pages/DrinksPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../components/Footer';
 
-const DrinksPage = () => (
-  <div>
-    PAGINA DO DRINKSPAGE
-    <Footer />
-  </div>
-);
+function DrinksPage() {
+  return (
+    <div>
+      <h1>PAGINA DO DRINKSPAGE</h1>
+      <Footer />
+    </div>
+  );
+}
 
 export default DrinksPage;
