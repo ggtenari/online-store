8039ca5ef854f33f0d0353673c6bda7b1c4b13e5
Pamela <abrepamm@gmail.com>
2022-07-15 15:35:50 -0300

    req-16-componente footer criado

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import drinkicon from '../images/drinkIcon.svg';
+
+function Footer() {
+  return (
+    <div data-testid="footer">
+      <link>
+        <img src={drinkicon} />
+      </link>
+      footer
+    </div>
+  );
+}
+
+export default Footer;
