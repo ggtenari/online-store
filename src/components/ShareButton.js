ef079a593f47e4f798977af554c4328a17a73b11
Pamela <abrepamm@gmail.com>
2022-07-20 21:04:53 -0300

    função copy criada

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import copy from 'clipboard-copy';
+import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
 
-function ShareButton() {
-  const handleOnClick = () => 'click';
+function ShareButton({ url }) {
+  const copyToClipboard = () => {
+    copy(url);
+    alert('Link copied!');
+  };
   return (
     <input
-      onClick={ handleOnClick }
+      onClick={ copyToClipboard }
       type="image"
       src={ shareIcon }
       alt="Share icon"
     />
   );
 }
+
+ShareButton.propTypes = {
+  url: PropTypes.string.isRequired,
+};
 
 export default ShareButton;
