81f3f70256f4d5ef00893dc696e8813490eae533
MariFilgueiras <filgueirasaraujo@gmail.com>
2022-07-22 15:45:50 -0300

    inicio pagina

--- src/pages/DoneRecipesPage.jsx
+++ src/pages/DoneRecipesPage.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
 import Header from '../components/Header';
 
-function DoneRecipesPage() {
+export default function DoneRecipesPage() {
   return (
-    <div>
+    <>
       <Header title="Done Recipes" />
-    </div>
+      <div>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          name="all"
+        // onClick={ handleFilter }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          name="food"
+        // onClick={ handleFilter }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          name="drink"
+        // onClick={ handleFilter }
+        >
+          Drinks
+        </button>
+      </div>
+      <div key="">
+        <span
+          data-testid={ `${index}-horizontal-name` }
+        >
+          { Name }
+        </span>
+        <img
+          data-testid={ `${index}-horizontal-image` }
+          src={ image }
+          alt="card-recipe"
+          // onClick={}
+        />
+        <p
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          {/* condição para categoria,nacionalidade,alcoolico */}
+        </p>
+        <p
+          data-testid={ `${index}-horizontal-done-date` }
+        >
+          {/* condição para data */}
+        </p>
+        {/* botões e filtros */}
+
+      </div>
+    </>
+
   );
 }
-
-export default DoneRecipesPage;
