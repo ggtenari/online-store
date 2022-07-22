043cdf6e29790594091ebc29bae3fe738b6f1c42
Kennethy <kennethymov@gmail.com>
2022-07-22 14:14:18 -0300

    merge branchs

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { useRecipeApp } from '../context/RecipeAppProvider';
 // import StartRecipe from './StartRecipe';
 
