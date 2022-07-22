32ab92feedbfc2ba77ed3018f5079c3eec6ea087
MariFilgueiras <filgueirasaraujo@gmail.com>
2022-07-22 16:35:43 -0300

    cont-page

--- src/pages/DoneRecipesPage.jsx
+++ src/pages/DoneRecipesPage.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default function DoneRecipesPage() {
+  const [cards, setCards] = useState([]);
+  const [allCards, setAllCards] = useState([]);
+
+  const handleFilter = ({ target }) => {
+    if (target.name === 'all') setCards(allCards);
+    else setCards(allCards.filter((card) => card.type === target.name));
+  };
   return (
     <>
       <Header title="Done Recipes" />
@@ -10,7 +17,7 @@ export default function DoneRecipesPage() {
           data-testid="filter-by-all-btn"
           type="button"
           name="all"
-        // onClick={ handleFilter }
+          onClick={ handleFilter }
         >
           All
         </button>
@@ -18,7 +25,7 @@ export default function DoneRecipesPage() {
           data-testid="filter-by-food-btn"
           type="button"
           name="food"
-        // onClick={ handleFilter }
+          onClick={ handleFilter }
         >
           Food
         </button>
@@ -26,35 +33,40 @@ export default function DoneRecipesPage() {
           data-testid="filter-by-drink-btn"
           type="button"
           name="drink"
-        // onClick={ handleFilter }
+          onClick={ handleFilter }
         >
           Drinks
         </button>
       </div>
-      <div key="">
-        <span
-          data-testid={ `${index}-horizontal-name` }
-        >
-          { Name }
-        </span>
-        <img
-          data-testid={ `${index}-horizontal-image` }
-          src={ image }
-          alt="card-recipe"
-          // onClick={}
-        />
-        <p
-          data-testid={ `${index}-horizontal-top-text` }
-        >
-          {/* condição para categoria,nacionalidade,alcoolico */}
-        </p>
-        <p
-          data-testid={ `${index}-horizontal-done-date` }
-        >
-          {/* condição para data */}
-        </p>
-        {/* botões e filtros */}
-
+      <div>
+        {cards && cards.map((card, index) => (
+          <div key="">
+            <span
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { Name }
+            </span>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ image }
+              alt="card-recipe"
+            // onClick={}
+            />
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              { card.type === 'food' ? (
+                `${card.nationality} - ${card.category}`
+              ) : card.alcoholicOrNot }
+            </p>
+            <p
+              data-testid={ `${index}-horizontal-done-date` }
+            >
+              {/* condição para data */}
+            </p>
+            {/* botões e filtros */}
+          </div>
+        ))}
       </div>
     </>
 
