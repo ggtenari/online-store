aacd9930fd18ee00a71c1807f22d153926403607
Alencar Dias Da Costa <alencardiascosta@hotmail.com>
2022-07-25 16:40:30 -0300

    testes andando

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../helpers/renderWithRouter';
+import renderWithRouter from '..helpers/renderWithRouter';
 import App from '../App';
+import RecipeAppProvider from '../context/RecipeAppProvider';
+
+describe('Testando o componente Header', () => {
+  it('Verificando os elementos na tela', () => {
+    const { history } = renderWithRouter(<RecipeAppProvider><App /></RecipeAppProvider>);
+
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const pageTitle = screen.getByTestId('page-title');
+    const searchIcon = screen.getByTestId('search-top-btn');
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(pageTitle).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+  });
+
+  it('Verificando se a barra de pesquisa aparece ao clicar no botão', () => {
+    const { history } = renderWithRouter(<RecipeAppProvider><App /></RecipeAppProvider>);
+
+    history.push('/foods');
+
+    const dataTestIdSearch = 'search-input';
+
+    const searchIcon = screen.getByTestId('search-top-btn');
+
+    expect(screen.queryByTestId(dataTestIdSearch)).not.toBeInTheDocument();
+
+    userEvent.click(searchIcon);
+
+    expect(screen.getByTestId(dataTestIdSearch)).toBeInTheDocument();
+
+    userEvent.click(searchIcon);
+
+    expect(screen.queryByTestId(dataTestIdSearch)).not.toBeInTheDocument();
+  });
+});
 
 describe('7 - Implemente o header de acordo com a necessidade de cada tela', () => {
   it('testa elementos', () => {
-    const { history } = renderWithRouter(<App />);
-
-    history.push('/foods:id');
-    
-    const titleIconInput = screen.getByTestId('profile-top-btn');
-    const searchIconInput = screen.getByTestId('search-top-btn');
-    const titleInput = screen.getByTestId('page-title');
-
-    expect(titleIconInput).toBeInTheDocument()
-    expect(searchIconInput).not.toBeInTheDocument()
-    expect(titleInput).toBeInTheDocument()
-  })
-
-  it('testa rota com render', () => {
-    const { history } = renderWithRouter(<App />);
-
+    const { history } = renderWithRouter(<RecipeAppProvider><App /></RecipeAppProvider>);
     history.push('/foods');
-    
+
     const titleIconInput = screen.getByTestId('profile-top-btn');
     const searchIconInput = screen.getByTestId('search-top-btn');
     const titleInput = screen.getByTestId('page-title');
@@ -32,61 +52,14 @@ describe('7 - Implemente o header de acordo com a necessidade de cada tela', () 
     expect(searchIconInput).toBeInTheDocument()
     expect(titleInput).toBeInTheDocument()
 
-  });
-});
+    userEvent.click(searchIconInput)
 
-  describe('Redirecione a pessoa usuária para a tela de perfil ao clicar no botão de perfil', () => {
-  it('testa redirecionamento', () => {
-    const {history} = renderWithRouter(<App />)
+    const searchInput = screen.getByTestId('search-input');
 
-    localStorage.setItem('name', JSON.stringify({ email: 'trybe@teste.com' }));
-    history.push('/foods');
-    const enterButton = screen.getByTestId('profile-top-btn')
-    userEvent.click(enterButton)
-    expect(history.location.pathname).toBe('/profile');
-})
-  it('o header renderiza sem o search', () => {
-  renderWithRouter(<App />);
-  
-  const titleIconInput = screen.getByTestId('profile-top-btn')
-  const searchIconInput = screen.getByTestId('search-top-btn')
-  const titleInput = screen.getByTestId('page-title')
+    expect(searchInput).toBeInTheDocument()
 
-    expect(titleIconInput).toBeInTheDocument()
-    expect(searchIconInput).not.toBeInTheDocument()
-    expect(titleInput).toBeInTheDocument()
-  });
-});
+    userEvent.click(searchIconInput)
 
-describe('9 - Desenvolva o botão de busca que, ao ser clicado, a barra de busca deve aparecer. O mesmo serve para escondê-la', () => {
-  it('Verificando se a barra de pesquisa aparece ao clicar no botão', () => {
-    const { history } = renderWithRouter(<App />);
-
-    history.push('/foods');
-    const textSearch = screen.getByTestId('search-input');
-    const searchIconInput = screen.getByTestId('search-top-btn')
-    expect(textSearch).not.toBeInTheDocument();
-
-    userEvent.click(searchIconInput);
-    expect(textSearch).toBeInTheDocument();
-    userEvent.click(searchIconInput);
-    expect(textSearch).not.toBeInTheDocument();
-  });
-  
-  it('Verifica os inputs radio', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/foods');
-
-    const searchIconInput = screen.getByTestId('search-top-btn');
-    userEvent.click(searchIcon);
-
-    expect(screen.queryByTestId('search-input')).toBeInTheDocument();
-    expect(screen.getByTestId('ingredient-search-radio')).toBeInTheDocument();
-    expect(screen.getByTestId('name-search-radio')).toBeInTheDocument();
-    expect(screen.getByTestId('first-letter-search-radio')).toBeInTheDocument();
-    expect(screen.getByTestId('exec-search-btn')).toBeInTheDocument();
-
-    userEvent.click(searchIconInput);
-    expect(screen.queryByTestId('search-input')).not.toBeInTheDocument();
+    expect(searchInput).not.toBeInTheDocument()
   });
 });
--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,67 +1,144 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from './renderWithRouter';
-import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+import Profile from '../pages/Profile';
+import App from '../App'
 
+describe('57, 59 - Implemente os elementos da tela de perfil', () => {
+  it('Todos o data-testid do email e de todos os botões', () => {
+    const { history } = renderWithRouter(<Profile />)
 
-describe('Testar o componente Profile', () => {
-  it('testar elementos renderizados',  () => {
-    renderWithRouter(<Profile />);
-    const email = screen.getByTestId('/profile-email/i')
-    expect(email).toBeInTheDocument();
+    const userEmail = screen.getByTestId('profile-email');
+    const doneRecipesButton = screen.getByTestId('profile-done-btn');
+    const favoriteButton = screen.getByTestId('profile-favorite-btn');
+    const logoutButton = screen.getByTestId('profile-logout-btn');
 
-    const doneRecipe = screen.getByTestId('/profile-done-btn/i')
-    expect(doneRecipe).toBeInTheDocument();
-
-    const favoriteRecipe = screen.getByTestId('profile-favorite-btn/i');
-    expect(favoriteRecipe).toBeInTheDocument();
-
-    const logout = screen.getByTestId('profile-logout-btn');
-    expect(logout).toBeInTheDocument();
-  });
-
-  it('Funcionamento do botão doneRecipe', async() => {
-    const { history } =renderWithRouter(<Profile />);
-    const doneRecipe = screen.getByTestId('/profile-done-btn/i')
-
-    userEvent.click(doneRecipe);
-
-    await waitFor(() => expect(history.location.pathname).toBe('/done-recipe'));
-  });
-
-  it('Funcionamento do botão favoriteRecipe', async() => {
-    const { history } =renderWithRouter(<Profile />);
-    const favoriteRecipe = screen.getByTestId('/profile-favorite-btn/i')
-
-    userEvent.click(favoriteRecipe);
-
-    await waitFor(() => expect(history.location.pathname).toBe('/favorite-recipe/i'));
-  });
-
-  it('Funcionamento do botão logout', async() => {
-    const { history } =renderWithRouter(<Profile />);
-    const logout = screen.getByTestId('profile-logout-btn/i');
-
-    userEvent.click(logout);
-    localStorage.clear()
-
-    await waitFor(() => expect(history.location.pathname).toBe('/'));
-  });
-
-  
-
-
-
-
-
-
-    // history.push('/profile');
-    // const logout = screen.getByTestId('profile-logout-btn');
-    // userEvent.click(logout);
-    // localStorage.clear();
-    // // history.push('/');
-    // expect(history.location.pathname).toBe('/');
-
+    expect(userEmail).toBeInTheDocument();
+    expect(doneRecipesButton).toBeInTheDocument();
+    expect(favoriteButton).toBeInTheDocument();
+    expect(logoutButton).toBeInTheDocument();
   })
+}); 
 
+describe('58 - Email visível', () => {
+  it('testa se o email tá visível', () => {
+  const { history } = renderWithRouter(<App />)
+        const emailInput = screen.getByTestId('email-input')
+        const passwordInput = screen.getByTestId('password-input')
+        userEvent.type(emailInput, 'email@mail.com')
+        userEvent.type(passwordInput, '1234567')
+        const btnLogin = screen.getByTestId('login-submit-btn')
+        expect(btnLogin).toBeInTheDocument()
+        expect(btnLogin).toBeEnabled()
+        userEvent.click(btnLogin)
+
+        expect(screen.getByTestId('page-title')).toBeInTheDocument()
+        history.push('/profile')
+        const userEmail = screen.getByTestId('profile-email')
+        expect(userEmail).toBeInTheDocument()
+        
+  })
+}); 
+
+describe('60 até 62 - Redirecionamentos botões - fav, done recipes e logout', () => {
+  it('Redireciona Done Recipes', () => {
+    const { history } = renderWithRouter(<Profile />)
+
+    const doneRecipesButton = screen.getByTestId('profile-done-btn');
+    userEvent.click(doneRecipesButton);
+    expect(history.location.pathname).toBe('/done-recipes');
+  })
+  it('Redireciona Favorite', () => {
+    const { history } = renderWithRouter(<Profile />)
+
+    const favoriteButton = screen.getByTestId('profile-favorite-btn');
+    userEvent.click(favoriteButton);
+    expect(history.location.pathname).toBe('/favorite-recipes');
+  })
+  it('Redireciona logout e limpa local storage', () => {
+    const { history } = renderWithRouter(<Profile />)
+
+    const logoutButton = screen.getByTestId('profile-logout-btn');
+    userEvent.click(logoutButton);
+    expect(history.location.pathname).toBe('/');
+  })
+}); 
+
+describe('Testando página Profile', () => {
+  it('57 - Testa os elementos descritos no protótipo', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/profile');
+
+    const email = screen.getByTestId('profile-email');
+    const doneRecipesButton = screen.getByTestId('profile-done-btn');
+    const favoriteButton = screen.getByTestId('profile-favorite-btn');
+    const logoutButton = screen.getByTestId('profile-logout-btn');    
+
+    expect(email).toBeInTheDocument();
+    expect(doneRecipesButton).toBeInTheDocument();
+    expect(favoriteButton).toBeInTheDocument();
+    expect(logoutButton).toBeInTheDocument();
+  });
+
+  /* it('58 - E-mail do usuário visível', () => {
+    const { history } = renderWithRouter(<App />);
+  }) */
+
+  it('59 - E-mail do usuário visível', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const email = screen.getByRole('button',
+    {name: /Done Recipes/i});
+    const favoriteButton = screen.getByRole('button',
+    {name: /Favorite Recipes/i});
+    const logoutButton = screen.screen.getByRole('button',
+    {name: /Logout/i}); 
+
+    expect(email).toBeInTheDocument()
+    expect(favoriteButton).toBeInTheDocument()
+    expect(logoutButton).toBeInTheDocument()
+  })
+  it('60 - Testando se ao clicar em Done Recipes é redirecionado corretamente', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/profile');
+    const doneRecipesButton = screen.getByTestId('profile-done-btn');
+    userEvent.click(doneRecipesButton);
+    expect(history.location.pathname).toBe('/done-recipes');
+  });
+
+  it('61 - Testando se ao clicar em Favorite Recipes é redirecionado corretamente', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/profile');
+    const favoriteButton = screen.getByTestId('profile-favorite-btn');
+    userEvent.click(favoriteButton);
+    expect(history.location.pathname).toBe('/favorite-recipes');
+  });
+
+  it('62 - Testando se ao clicar em Logout é redirecionado e o localStorage é limpo', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/profile');
+
+    localStorage.setItem('user', '{ "email": "email@mail.com" }');
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken', '1');
+    localStorage.setItem('doneRecipes', '[]');
+    localStorage.setItem('favoriteRecipes', '[]');
+    localStorage.setItem('inProgressRecipes', '{}');
+
+    const logoutButton = screen.getByTestId('profile-logout-btn');
+    userEvent.click(logoutButton);
+    expect(history.location.pathname).toBe('/');
+
+    expect(localStorage.getItem('email')).toBeNull();
+    expect(localStorage.getItem('mealsToken')).toBeNull();
+    expect(localStorage.getItem('cocktailsToken')).toBeNull();
+    expect(localStorage.getItem('doneRecipes')).toBeNull();
+    expect(localStorage.getItem('favoriteRecipes')).toBeNull();
+    expect(localStorage.getItem('inProgressRecipes')).toBeNull();
+  });
+});
