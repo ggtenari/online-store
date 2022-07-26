1fcfc4a6aa1dc9a4971be3b6b43d31bbcaad4bc3
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-07-26 15:46:48 -0300

    foodpage

--- src/pages/ProgressRecipeFoodPage.jsx
+++ src/pages/ProgressRecipeFoodPage.jsx
@@ -1,36 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import RecipeDetails from '../components/RecipeDetails';
 import { useRecipeApp } from '../context/RecipeAppProvider';
 import fetchDetailsFood from '../helpers/fetchDetailsFood';
 
 const ProgressRecipeFoodPage = ({ match: { params: { id } } }) => {
-  const { setPage, details,
+  const { details,
     ingredientsInProgress,
-    foodCheck,
-    setFoodCheck,
     setDetails, setIngredientsInProgress,
   } = useRecipeApp();
 
-  const localStorageObject = {};
-  const [styleObject, setStyleObject] = useState({});
+  let receitasEmProgresso = [];
+
+  if (localStorage.getItem('inProgressRecipes')) {
+    receitasEmProgresso = JSON
+      .parse(localStorage.getItem('inProgressRecipes'))
+      .meals[id] ? JSON.parse(localStorage
+        .getItem('inProgressRecipes')).meals[id]
+      : [];
+  }
+
+  const [localStorageArray,
+    setLocalStorageArray] = useState(receitasEmProgresso);
+
+  useEffect(() => { console.log(localStorageArray); }, [localStorageArray]);
+  const [finished, setFinished] = useState(false);
 
   const setChecked = (value) => {
-    const prevValue = JSON.parse(localStorage.getItem(`${details.strMeal}InProgress`));
-    if (prevValue && foodCheck) {
-      prevValue[value] = !foodCheck[value];
+    if (localStorageArray.includes(value)) {
+      const prevLocalStorage = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      setLocalStorageArray(localStorageArray
+        .filter((ingredient) => ingredient !== value));
+      prevLocalStorage.meals[id] = localStorageArray
+        .filter((ingredient) => ingredient !== value);
+      localStorage.setItem('inProgressRecipes', JSON.stringify(prevLocalStorage));
+    } else {
+      const prevLocalStorage = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      console.log(prevLocalStorage);
+      prevLocalStorage.meals[id] = [...localStorageArray, value];
+      setLocalStorageArray([...localStorageArray, value]);
+      localStorage.setItem('inProgressRecipes', JSON.stringify(prevLocalStorage));
     }
-    localStorage.setItem((`${details.strMeal}InProgress`), JSON
-      .stringify(prevValue));
-    setFoodCheck((prev) => ({
-      ...prev,
-      [value]: !prev[value],
-    }));
   };
 
+  const checkChecked = (value) => (!!localStorageArray.includes(value));
   const style = {
     width: '150px',
     heigth: '150px',
+  };
+
+  const finishRecipe = () => {
+    const local = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    delete local.meals[id];
+    localStorage.setItem('inProgressRecipes', JSON.stringify(local));
+    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    doneRecipes.push(id);
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
   };
 
   useEffect(() => {
@@ -49,28 +74,25 @@ const ProgressRecipeFoodPage = ({ match: { params: { id } } }) => {
             .filter(([key, value]) => key.includes('Measure') && value !== '')));
       setIngredientsInProgress({ ingredientList, measureList });
     });
-  }, [id]);
+    if (!localStorage.getItem('doneRecipes')) {
+      localStorage.setItem('doneRecipes', JSON.stringify([]));
+    }
+    if (!localStorage.getItem('inProgressRecipes')) {
+      localStorage.setItem('inProgressRecipes', JSON
+        .stringify({ meals: {}, cocktails: {} }));
+    }
+  }, []);
 
   useEffect(() => {
-    if (details && localStorage.getItem(`${details.strMeal}InProgress`)) {
-      const objLocal = JSON.parse(localStorage.getItem(`${details.strMeal}InProgress`));
-      setFoodCheck(objLocal);
-    } else if (ingredientsInProgress.ingredientList) {
-      ingredientsInProgress.ingredientList.forEach((ingrediente) => {
-        localStorageObject[ingrediente] = false;
-        setStyleObject({ ...styleObject, [ingrediente]: {} });
-        setFoodCheck((prev) => ({
-          ...prev,
-          [ingrediente]: false,
-        }));
-      });
-      localStorage.setItem((`${details.strMeal}InProgress`), JSON
-        .stringify(localStorageObject));
+    if (ingredientsInProgress.ingredientList) {
+      if (ingredientsInProgress
+        .ingredientList.length === localStorageArray.length) {
+        setFinished(true);
+      } else {
+        setFinished(false);
+      }
     }
-  }, [details]);
-  useEffect(() => {
-    setPage('foodInProgress');
-  }, []);
+  }, [localStorageArray]);
 
   return (
     <div>
@@ -92,39 +114,49 @@ const ProgressRecipeFoodPage = ({ match: { params: { id } } }) => {
                 <div>
                   {
                     ingredientsInProgress.ingredientList.map((ingrediente, index) => {
-                      if (foodCheck && ingrediente !== null) {
-                        <div key={ index }>
-                          <label
-                            data-testid={ `${index}-ingredient-step` }
-                            htmlFor={ ingrediente }
-                          >
-                            { foodCheck[ingrediente]
-                              ? (
-                                <s>
-                                  {(
-                                    `${ingrediente}${ingredientsInProgress
-                                      .measureList[index]}`
-                                  )}
-                                </s>)
-                              : `${ingrediente} ${ingredientsInProgress
-                                .measureList[index]}`}
-                            <input
-                              type="checkbox"
-                              checked={ foodCheck[ingrediente] }
-                              value={ ingrediente }
-                              id={ ingrediente }
-                              onChange={ (event) => setChecked(event.target.value) }
+                      if (ingrediente !== null) {
+                        return (
+                          <div key={ index }>
+                            <label
+                              data-testid={ `${index}-ingredient-step` }
+                              htmlFor={ ingrediente }
+                            >
+                              { checkChecked(ingrediente)
+                                ? (
+                                  <s>
+                                    {(
+                                      `${ingrediente}${ingredientsInProgress
+                                        .measureList[index]}`
+                                    )}
+                                  </s>)
+                                : `${ingrediente} ${ingredientsInProgress
+                                  .measureList[index]}`}
+                              <input
+                                type="checkbox"
+                                checked={ checkChecked(ingrediente) }
+                                value={ ingrediente }
+                                id={ ingrediente }
+                                onChange={ () => setChecked(ingrediente) }
 
-                            />
-                          </label>
+                              />
+                            </label>
 
-                        </div>;
+                          </div>
+                        );
                       }
+                      return null;
                     })
                   }
-                  <button type="button" data-testid="finish-recipe-btn">
-                    finalizar
-                  </button>
+                  <Link to="/done-recipes">
+                    <button
+                      type="button"
+                      data-testid="finish-recipe-btn"
+                      disabled={ !finished }
+                      onClick={ finishRecipe }
+                    >
+                      finalizar
+                    </button>
+                  </Link>
                 </div>
               ) }
 
@@ -137,6 +169,11 @@ const ProgressRecipeFoodPage = ({ match: { params: { id } } }) => {
 };
 
 ProgressRecipeFoodPage.propTypes = {
-  match: PropTypes.objectOf(PropTypes.string).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
 };
+
 export default ProgressRecipeFoodPage;
